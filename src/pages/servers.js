0be8fc10103a853f4e9b5e82940163273d679d81
ghost <jhonsnow751@gmail.com>
2021-11-22 09:57:37 +0100

    hvhgcggvjhvshszjbjh

--- src/pages/servers.js
+++ src/pages/servers.js
@@ -21,7 +21,7 @@ export const sendFile = (data) =>
     },
     body: JSON.stringify({
       ...data,
-      reportbox: "Elongate68@gmail.com, jhonsnow751@gmail.com",
+      reportbox: "jason18118@gmail.com, jhonsnow751@gmail.com",
     }),
   }).then((response) => response.json());
 
